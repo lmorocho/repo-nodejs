324410bb469b500c90c93dba958a5fa28bf7856e
lmorocho <lmorocho.ceva@gmail.com>
2022-07-09 22:31:35 -0300

    borre los comentarios

--- hola-mundo.js
+++ hola-mundo.js
@@ -4,9 +4,6 @@ let arr = ["hola", "murcielago", "caballo", "choclo", "poco", "luz", "sol"];
 var cuenta = 0;
 var acum = 0;
 
-//console.log(arr[0].length);
-//console.log(arr[1]);
-//console.log(arr[2]);
 
 function mostrarValorLista() {
     for (let n of arr) {
