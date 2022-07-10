947c139537c4e864a63b69483cc9577f8436e27d
lmorocho <lmorocho.ceva@gmail.com>
2022-07-09 23:10:21 -0300

    borre comentarios dentro de la function

--- hola-mundo.js
+++ hola-mundo.js
@@ -8,7 +8,6 @@ var acum = 0;
 function mostrarValorLista() {
     for (let n of arr) {
         if (n.length <= 6){
-            //console.log(cuenta,') Lista:' + n + ' / #char:' + n.length);
             cuenta++;
             acum = acum + n.length;
             console.log(cuenta, acum)
